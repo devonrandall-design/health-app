3ab48438cdbca65c96775d04e76adddc8a14d97d
devonrandall-design <111830060+devonrandall-design@users.noreply.github.com>
2022-09-22 20:34:19 -0400

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 import Footer from './footer.js'
 
 export default function App() {
+
+const month = new Date().toLocaleString(
+  'default', {month: 'long'}
+);
+
+const day = new Date().getDate() 
+const year = new Date().getFullYear() 
+const time = new Date().getHours() + ':' + new Date().getMinutes()
+
+
+
+console.log(time)
+
+
+
   return (
     <div className="app-container">
       <div className="header">
@@ -10,10 +25,12 @@ export default function App() {
       <p>This is going to get better</p>
       </div>
       <div className="sub-header">
-        <h3>September 22, 2022</h3>
-        <p>12:35 am</p>
+        <h3>{month} {day}, {year}</h3>
+        <p>{time}</p>
         </div>
+        <div className="entry-header">
         <h3 className="entry-title">Entries</h3>
+        </div>
       <div className="mainbody">
       
       </div>
